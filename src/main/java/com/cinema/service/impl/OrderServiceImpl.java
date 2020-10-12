476bbebc2e1bc4627d606484e274aceb57991150
Roman <matviichuk.office@gmail.com>
2020-10-12 11:18:07 +0300

    fixed completeOrder();

--- src/main/java/com/cinema/service/impl/OrderServiceImpl.java
+++ src/main/java/com/cinema/service/impl/OrderServiceImpl.java
@@ -1,13 +1,13 @@
 package com.cinema.service.impl;
 
 import com.cinema.dao.OrderDao;
-import com.cinema.dao.ShoppingCartDao;
 import com.cinema.lib.Inject;
 import com.cinema.lib.Service;
 import com.cinema.model.Order;
 import com.cinema.model.Ticket;
 import com.cinema.model.User;
 import com.cinema.service.OrderService;
+import com.cinema.service.ShoppingCartService;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -16,7 +16,7 @@ public class OrderServiceImpl implements OrderService {
     @Inject
     private OrderDao orderDao;
     @Inject
-    private ShoppingCartDao shoppingCartDao;
+    private ShoppingCartService shoppingCartService;
 
     @Override
     public Order completeOrder(User user, List<Ticket> tickets) {
@@ -24,7 +24,7 @@ public class OrderServiceImpl implements OrderService {
         order.setUser(user);
         order.setTickets(tickets);
         order.setOrderDate(LocalDateTime.now());
-        shoppingCartDao.update(shoppingCartDao.getByUser(user));
+        shoppingCartService.clear(shoppingCartService.getByUser(user));
         return orderDao.create(order);
     }
 
